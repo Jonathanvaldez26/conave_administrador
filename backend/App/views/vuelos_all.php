1cb782a3a4dc49f7ae12b0b8389beb3d2419ca2a
Gerson Velasco <gvelassco@gmail.com>
2022-03-29 17:53:07 -0600

    buscador

--- backend/App/views/vuelos_all.php
+++ backend/App/views/vuelos_all.php
@@ -773,7 +773,7 @@
             </div>
         </div>
 
-        <div class="modal fade" id="Modal_Add_Itinerario" tabindex="-1" role="dialog" aria-labelledby="" aria-hidden="true">
+        <div class="modal fade" id="Modal_Add_Itinerario" role="dialog" aria-labelledby="" aria-hidden="true">
             <div class="modal-dialog modal-lg" role="document">
                 <div class="modal-content">
                     <div class="modal-header">
@@ -1112,7 +1112,7 @@
 <script>
     $(document).ready(function() {
 
-       // $('.select_2').select2();
+       $('.select_2').select2();
 
        $('#itinerario-tabla').DataTable({
         "drawCallback": function( settings ) {
